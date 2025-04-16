bb384b964a5cbe3bb2abb618eacc185dcecb84e8
Heikel Molina <qbah3h@gmail.com>
2025-04-16 00:06:49 -0300

    Added second box for comparison and updates in the calculations

--- index.js
+++ index.js
@@ -8,30 +8,38 @@ function calculate(formId) {
 
     const eficacia = ra / re;
     const eficiencia = (ra / (ca * ta)) / (re / (ce * te));
-    const efectividad = (getPuntaje(eficacia * 100) + getPuntaje(eficiencia * 100)) / 2;
+    const efectividad = ((getPuntajeEficacia(eficacia * 100) + getPuntajeEficiencia(eficiencia)) / 2) / 5; // /5 es el numero maximo puuntaje
 
-    document.getElementById(`eficacia${formId}`).textContent = `${(eficacia * 100).toFixed(2)}% (${getPuntaje(eficacia * 100)} puntos)`;
-    document.getElementById(`eficiencia${formId}`).textContent = `${(eficiencia * 100).toFixed(2)}% (${getPuntaje(eficiencia * 100)} puntos)`;
-    document.getElementById(`efectividad${formId}`).textContent = `${efectividad.toFixed(2)} puntos`;
-    
+    document.getElementById(`eficacia${formId}`).textContent = `${(eficacia * 100).toFixed(2)}% (${getPuntajeEficacia(eficacia * 100)} puntos)`;
+    document.getElementById(`eficiencia${formId}`).textContent = `${(eficiencia).toFixed(2)} (${getPuntajeEficiencia(eficiencia)} puntos)`;
+    document.getElementById(`efectividad${formId}`).textContent = `${(efectividad * 100).toFixed(0)}%`;
+
     document.getElementById(`results${formId}`).classList.remove('hidden');
 }
 
-document.getElementById('calcForm1').addEventListener('submit', function(e) {
+document.getElementById('calcForm1').addEventListener('submit', function (e) {
     e.preventDefault();
     calculate('1');
 });
 
-document.getElementById('calcForm2').addEventListener('submit', function(e) {
+document.getElementById('calcForm2').addEventListener('submit', function (e) {
     e.preventDefault();
     calculate('2');
 });
 
-function getPuntaje(porcentaje) {
-    if (porcentaje <= 20) return 5;
-    if (porcentaje <= 40) return 4;
-    if (porcentaje <= 60) return 3;
-    if (porcentaje <= 80) return 2;
-    if (porcentaje <= 90) return 1;
-    return 0;
+// eficacia recibe un numero que es el porcentaje
+function getPuntajeEficacia(porcentaje) {
+    if (porcentaje <= 20) return 0;
+    if (porcentaje <= 40) return 1;
+    if (porcentaje <= 60) return 2;
+    if (porcentaje <= 80) return 3;
+    if (porcentaje <= 90) return 4;
+    return 5;
+}
+
+// eficiencia recibe un numero que puede ser decimal
+function getPuntajeEficiencia(porcentaje) {
+    if (porcentaje > 1) return 5;
+    if (porcentaje == 1) return 3;
+    if (porcentaje < 1) return 1;
 }
